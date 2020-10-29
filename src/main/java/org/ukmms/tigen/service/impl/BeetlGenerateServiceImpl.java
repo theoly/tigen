5f27178f9c9846a7f15d346c0560e88f405f6d57
theoly <theo@ukmms.org>
2020-10-29 10:56:13 +0800

    beetl

--- src/main/java/org/ukmms/tigen/service/impl/BeetlGenerateServiceImpl.java
+++ src/main/java/org/ukmms/tigen/service/impl/BeetlGenerateServiceImpl.java
@@ -1,7 +1,12 @@
 package org.ukmms.tigen.service.impl;
 
+import org.beetl.core.Configuration;
+import org.beetl.core.GroupTemplate;
+import org.beetl.core.Template;
+import org.beetl.core.resource.StringTemplateResourceLoader;
 import org.ukmms.tigen.service.GenerateService;
 
+import java.io.IOException;
 import java.util.Map;
 
 /**
@@ -10,7 +15,16 @@ import java.util.Map;
  */
 public class BeetlGenerateServiceImpl implements GenerateService {
     @Override
-    public String generate(String template, Map<String, Object> info) {
-        return null;
+    public String generate(String template, Map<String, Object> param) throws IOException {
+        //初始化代码
+        StringTemplateResourceLoader resourceLoader = new StringTemplateResourceLoader();
+        Configuration cfg = Configuration.defaultConfiguration();
+        GroupTemplate gt = new GroupTemplate(resourceLoader, cfg);
+        //获取模板
+        Template t = gt.getTemplate(template);
+        t.binding(param);
+        //渲染结果
+        String str = t.render();
+        return str;
     }
 }
